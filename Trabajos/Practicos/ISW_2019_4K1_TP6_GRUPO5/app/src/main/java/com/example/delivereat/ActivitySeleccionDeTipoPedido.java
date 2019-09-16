de400198a96f8b9a99015735f8605cde45e6e56b
Agustin Garcia Rampini <aegr.developer@gmail.com>
2019-09-16 18:50:15 -0300

    Pantalla de seleccion de ubicación

--- Trabajos/Practicos/ISW_2019_4K1_TP6_GRUPO5/app/src/main/java/com/example/delivereat/ActivitySeleccionDeTipoPedido.java
+++ Trabajos/Practicos/ISW_2019_4K1_TP6_GRUPO5/app/src/main/java/com/example/delivereat/ActivitySeleccionDeTipoPedido.java
@@ -1,14 +1,24 @@
 package com.example.delivereat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.example.delivereat.util.DialogAlert;
+import com.google.android.gms.maps.model.LatLng;
 
 public class ActivitySeleccionDeTipoPedido extends AppCompatActivity {
 
-    private android.support.v7.widget.Toolbar toolbar;
+    private androidx.appcompat.widget.Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,11 +28,31 @@ public class ActivitySeleccionDeTipoPedido extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         if(getSupportActionBar() != null){
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_location);
+            //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            //getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_location);
             ((TextView) toolbar.findViewById(R.id.tvToolbar)).setText("Nuevo Pedido");
         }
 
+        findViewById(R.id.card_view_loquesea).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(ActivitySeleccionDeTipoPedido.this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                            ActivityPedidoLoQueSea.PERMISO_UBICACION);
+                }
+                else{
+                    iniciarPedidoDeLoQueSea();
+                }
+            }
+        });
+
+        findViewById(R.id.card_view_pedido).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               new DialogAlert(ActivitySeleccionDeTipoPedido.this, "¡Funcionalidad para grupos pares!").show();
+            }
+        });
 
 
     }
@@ -36,11 +66,47 @@ public class ActivitySeleccionDeTipoPedido extends AppCompatActivity {
             case android.R.id.home:
                 finish();
                 break;
+            case R.id.logout:
+                finish();
             default:
                 break;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_activity_pedidos, menu);
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case ActivityPedidoLoQueSea.PERMISO_UBICACION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    iniciarPedidoDeLoQueSea();
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+
+    }
+
+    private void iniciarPedidoDeLoQueSea(){
+        Intent intent = new Intent(ActivitySeleccionDeTipoPedido.this, ActivityPedidoLoQueSea.class);
+        startActivity(intent);
     }
 
 
